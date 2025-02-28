0d862c75abe73062c7e9dc0b7b6173cb6a9a6f05
SeifTdec <124317834+SeifTdec@users.noreply.github.com>
2025-02-28 11:16:19 +0200

    Add files via upload

--- people.py
+++ people.py
@@ -1,4 +1,6 @@
-from typing import Dict
+from typing import Dict, List
+from datetime import datetime
+from helper_classes import *
 
 class Person():
     def __init__(self, name: str, age: int, gender: str, contact_info: Dict, ID: int) -> None:
@@ -35,22 +37,48 @@ class Person():
 class Patient(Person):
     def __init__(self, name, age, gender, contact_info, ID, assigned_doctor, medical_history):
         super().__init__(name, age, gender, contact_info, ID)
-        self._medical_history: Dict = medical_history
+        self._medical_history: Dict = medical_history #changed when medical_history class is finished
         self._assigned_doctor = assigned_doctor
     def view_medical_history(self) -> None:
-        print(self._medical_history)
+        print(self._medical_history) #changed when medical_history class is finished
     
     def book_appointment(self, day, doctor) -> None:
-        return self._medical_history.update((day, doctor))
+        return self._medical_history.update((day, doctor)) #changed when appointment class is finished
 
     def get_patient_info(self) -> tuple[str, int, int]:
         return (self.get_name(), self.get_age(), self.get_id())
 
 class Doctor(Person):
+    def __init__(self, name, age, gender, contact_info, ID, specialization, available_slots, patients_list):
+        super().__init__(name, age, gender, contact_info, ID)
+        self._specialization = specialization
+        self._available_slots = available_slots
+        self._patients_list = patients_list
+    #work in progress getter/setter methods
     ...
-
+    
+    def diagnose_patient(self, patient):
+        diagnose = ()
+        return patient._medical_history((datetime.now() ,self.get_name(), diagnose)) #changed when medical_history class is finished
+    def prescribe_medication(self, patient,medication):
+        return (self.get_name(), patient.get_name(), medication) #work in progress
+    def view_patient_records(self, patient):
+        return patient.view_medical_history() #work in progress
+    
 class Nurse(Person):
-    ...
+    def __init__(self, name, age, gender, contact_info, ID, assigned_ward, patients_assigned):
+        super().__init__(name, age, gender, contact_info, ID)
+        self._assigned_ward = assigned_ward
+        self._patients_assigned: List = patients_assigned
+    def assist_doctor(self, doctor):
+        return (self.get_name(), doctor.get_name(), datetime.now())
+    def record_vitals(self, vitals:List):
+        return vitals #????
+    def update_patient_statues(self, patient):
+        statues = []
+        #make statues attribute in patient
+        return statues
+        
 
 class Administrator(Person):
     ...
